d880f3f4b322c5a7e05f0c23a7940073c7b02f4c
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-19 09:25:21 +0530

    added overloading

---
+++ Labs/Day-3/Day-3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Day_3
+{
+    //method overloading
+
+    class OverloadingDemo
+    {
+        public  void sum(int a,int b)
+        {
+            Console.WriteLine(a + b);   
+        }
+        public void sum(int a, int b,int c)
+        {
+            Console.WriteLine(a + b+c);
+        }
+        public void sum()
+        {
+            Console.WriteLine("I am empty");
+        }
+        public void sum(string a,string b)
+        {
+            Console.WriteLine(a+b);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           OverloadingDemo d=new OverloadingDemo();
+            d.sum(1,2);
+            d.sum(1, 2,3);
+            d.sum("Rahul","Sharma");
+        }
+    }
+}
