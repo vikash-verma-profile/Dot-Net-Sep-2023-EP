4f766332e387788d23011d2f2714b2e7234dd75e
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-21 09:32:46 +0530

    added code

---
+++ Labs/Day-5/Day-5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("C:\\Users\\om\\Desktop\\Dot-Net-Core\\Labs\\Day-5\\Day-5\\files\\test.txt");
+            sw.WriteLine("Vikash");
+            sw.Flush();
+            sw.Close();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
