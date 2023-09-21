5b67d66c09b70b74d20b0f5808575c3e179c33ab
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-21 09:50:03 +0530

    added code

--- Labs/Day-5/Day-5/Program.cs
+++ Labs/Day-5/Day-5/Program.cs
@@ -1,13 +1,27 @@
-﻿namespace Day_5
+﻿using System.Globalization;
+
+namespace Day_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("C:\\Users\\om\\Desktop\\Dot-Net-Core\\Labs\\Day-5\\Day-5\\files\\test.txt");
-            sw.WriteLine("Vikash");
+            string FilePath = "C:\\Users\\om\\Desktop\\Dot-Net-Core\\Labs\\Day-5\\Day-5\\files\\test.txt";
+            StreamWriter sw = new StreamWriter(FilePath);
+            Console.WriteLine("Please enter some value");
+            string valueToWriteInFile=Console.ReadLine();
+            sw.WriteLine(valueToWriteInFile);
             sw.Flush();
             sw.Close();
+
+            StreamReader sr = new StreamReader(FilePath);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+           string output= sr.ReadLine();
+            Console.WriteLine("Value from file is "+ output);
+            //while()
+            //{
+
+            //}
             Console.WriteLine("Hello, World!");
         }
     }
