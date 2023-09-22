b6052d65776af3d7b73a2d8c553f1a07db5c5c5c
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-22 09:19:52 +0530

    added

---
+++ Labs/Day-6/Day-6/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day_6
+{
+
+    class Test
+    {
+        public void Print()
+        {
+            Console.WriteLine("Print method");
+        }
+    }
+    static class TestExtension
+    {
+        static public void PrintNew(this Test test)
+        {
+            Console.WriteLine("Print new defination");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test=new Test();
+            test.Print();
+            test.PrintNew();
+        }
+    }
+}
