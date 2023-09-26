01d2658ab687b6ec20d09cf8d4344cd31e84ca17
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-26 10:16:56 +0530

    aded code

--- Labs/DBConnection/DBConnection/Program.cs
+++ Labs/DBConnection/DBConnection/Program.cs
@@ -4,14 +4,35 @@ namespace DBConnection
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void GetValueFromDB(BookDbContext db)
         {
-            BookDbContext db = new BookDbContext();
             var books = db.TblBooks;
             foreach (var item in books)
             {
                 Console.WriteLine(item.BookName);
             }
+        }
+
+        public static void AddDatatoDB(BookDbContext db)
+        {
+            Console.WriteLine("Please enter some book name: ");
+            TblBook book = new TblBook();
+            book.BookName = Console.ReadLine();
+            db.Add(book);
+            db.SaveChanges();
+
+        }
+        static void Main(string[] args)
+        {
+
+           
+            BookDbContext db = new BookDbContext();
+            AddDatatoDB(db);
+            GetValueFromDB(db);
+
+
+
+
         }
     }
 }
