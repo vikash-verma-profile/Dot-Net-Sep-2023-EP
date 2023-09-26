7ecbc224765eba8a618f694622c19d9215642d44
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-26 09:27:00 +0530

    aded code

---
+++ Labs/DBConnection/DBConnection/Models/TblBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBConnection.Models;
+
+public partial class TblBook
+{
+    public int Id { get; set; }
+
+    public string? BookName { get; set; }
+}
---
+++ Labs/DBConnection/DBConnection/Program.cs
@@ -0,0 +1,17 @@
+﻿using DBConnection.Models;
+
+namespace DBConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookDbContext db = new BookDbContext();
+            var books = db.TblBooks;
+            foreach (var item in books)
+            {
+                Console.WriteLine(item.BookName);
+            }
+        }
+    }
+}
