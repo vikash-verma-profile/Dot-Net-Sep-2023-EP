e0810cf79ad1f470ab7472a78c91871bbd3cff36
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-09-29 13:15:49 +0530

    First commit

---
+++ Labs/DemoWebApi/DemoWebApi/Controllers/DummyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DummyController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Vikash";
+        }
+    }
+}
